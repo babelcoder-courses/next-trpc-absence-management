7c0002567a408903adb0c550313592352f2099b5
Nuttavut Thongjor <nthongjor@LAPTOP-HEF902DE>
2023-05-10 11:16:11 +0700

    separate todo component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,61 @@
 import { type ChangeEventHandler, useState } from 'react'
 
-const IndexPage = () => {
-  const [todo, setTodo] = useState('')
-  const [todos, setTodos] = useState([
-    { id: 1, content: 'Todo#1' },
-    { id: 2, content: 'Todo#2' },
-    { id: 3, content: 'Todo#3' },
-  ])
+interface TodoFormProps {
+  addTodo: (todo: string) => void
+}
 
+const TodoForm = ({ addTodo }: TodoFormProps) => {
+  const [todo, setTodo] = useState('')
   const handleTodoFormChanged: ChangeEventHandler<HTMLInputElement> = (event) => {
     setTodo(event.target.value)
   }
-
-  const addTodo = () => {
-    setTodos([{ id: todos.length + 1, content: todo}, ...todos])
+  const handleAddTodo = () => {
+    addTodo(todo)
     setTodo('')
   }
 
   return (
     <>
       <input type="text" onChange={handleTodoFormChanged} value={todo} />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {
-          todos.map(todo => <li key={todo.id}>{todo.content}</li>)
-        }
-      </ul>
+      <button onClick={handleAddTodo}>Add Todo</button>
+    </>
+  )
+}
+
+interface Todo {
+  id: number;
+  content: string;
+}
+
+interface TodoListProps {
+  todos: Todo[]
+}
+
+const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <ul>
+      {
+        todos.map(todo => <li key={todo.id}>{todo.content}</li>)
+      }
+    </ul>
+  )
+}
+
+const IndexPage = () => {
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, content: 'Todo#1' },
+    { id: 2, content: 'Todo#2' },
+    { id: 3, content: 'Todo#3' },
+  ])
+
+  const addTodo = (todo: string) => {
+    setTodos([{ id: todos.length + 1, content: todo }, ...todos])
+  }
+
+  return (
+    <>
+      <TodoForm addTodo={addTodo}></TodoForm>
+      <TodoList todos={todos}></TodoList>
     </>
   )
 }
