cda9331cf7e4ccfa5cb398e65fe87260defc0ef9
Nuttavut Thongjor <nthongjor@LAPTOP-HEF902DE>
2023-05-10 14:45:27 +0700

    add exam solution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const useDocumentTitle = (title: string) => {
+function useCache<T>(key: string) {
+  const [value, setValue] = useState<T | null>(null);
+
+  const changeValue = (value: T) => {
+    setValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  const clear = () => {
+    setValue(null);
+    localStorage.removeItem(key);
+  };
+
   useEffect(() => {
-    document.title = title;
-  }, [title]);
-};
+    const item = localStorage.getItem(key);
+    setValue(item ? (JSON.parse(item) as T) : null);
+  }, [key]);
 
-interface UseLifeCycleHookProps {
-  onMount: () => void;
-  onUnmount: () => void;
+  return [value, changeValue, clear] as const;
 }
 
-const useLifecycleHooks = ({ onMount, onUnmount }: UseLifeCycleHookProps) => {
-  useEffect(() => {
-    onMount();
-
-    return () => onUnmount();
-  }, [onMount, onUnmount]);
-};
-
 const IndexPage = () => {
-  useDocumentTitle('Page Title');
-  useLifecycleHooks({
-    onMount: () => console.log('mounted!'),
-    onUnmount: () => console.log('unmounting!'),
-  });
+  const [value, setValue, clear] = useCache<string>('accessToken');
 
-  return <div></div>;
+  return (
+    <>
+      {/* ค่า value จะเปลี่ยนแปลงตลอดเมื่อเรียก setValue หรือ clear */}
+      {/* เมื่อคอมโพแนนท์ mounted ต้องดึงค่าล่าสุดของ value จาก localStorage ด้วย */}
+      {JSON.stringify(value)}
+      {/* เมื่อทำการคลิกจะเซ็ตค่า value เป็น 'my-access-token' */}
+      {/* และเก็บค่านี้ลง localStorage ภายใต้ key คือ accessToken */}
+      <button onClick={() => setValue('my-access-token')}>setValue</button>
+      {/* เมื่อทำการคลิกจะลบ value ออกจาก localStorage ใน key ที่ชื้อ accessToken */}
+      {/* พร้อมกำหนด value ปัจจุบันเป็น null เราจะเห็น value ใหม่บนหน้าจอเป็น null */}
+      <button onClick={() => clear()}>clear</button>
+    </>
+  );
 };
 
 export default IndexPage;
