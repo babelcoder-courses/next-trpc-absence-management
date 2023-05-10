9e5b6ffd6a7a5e987265fbff2cb6bf0a085773ae
Nuttavut Thongjor <nthongjor@LAPTOP-HEF902DE>
2023-05-10 14:01:11 +0700

    add useFetch hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,72 @@
-import { type ChangeEventHandler, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-interface TodoFormProps {
-  addTodo: (todo: string) => void
+interface DataItem {
+  id: number;
 }
 
-const TodoForm = ({ addTodo }: TodoFormProps) => {
-  const [todo, setTodo] = useState('')
-  const handleTodoFormChanged: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setTodo(event.target.value)
-  }
-  const handleAddTodo = () => {
-    addTodo(todo)
-    setTodo('')
-  }
+// React Hooks
+// function useFetch<T extends DataItem>(url: string) {
+const useFetch = <T extends DataItem>(url: string) => {
+  const [data, setData] = useState<T[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data = await (res.json() as Promise<T[]>);
+
+      setData(data);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return data;
+};
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const User = () => {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const users = useFetch<User>(url);
 
   return (
-    <>
-      <input type="text" onChange={handleTodoFormChanged} value={todo} />
-      <button onClick={handleAddTodo}>Add Todo</button>
-    </>
-  )
-}
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 interface Todo {
   id: number;
-  content: string;
+  title: string;
 }
 
-interface TodoListProps {
-  todos: Todo[]
-}
+const Todo = () => {
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  const todos = useFetch<Todo>(url);
 
-const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul>
-      {
-        todos.map(todo => <li key={todo.id}>{todo.content}</li>)
-      }
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 const IndexPage = () => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, content: 'Todo#1' },
-    { id: 2, content: 'Todo#2' },
-    { id: 3, content: 'Todo#3' },
-  ])
-
-  const addTodo = (todo: string) => {
-    setTodos([{ id: todos.length + 1, content: todo }, ...todos])
-  }
-
   return (
     <>
-      <TodoForm addTodo={addTodo}></TodoForm>
-      <TodoList todos={todos}></TodoList>
+      <User></User>
+      <br />
+      <Todo></Todo>
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
