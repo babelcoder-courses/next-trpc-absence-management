d16c6c90c5b073e06da44e713354b680d739f9b6
Nuttavut Thongjor <nthongjor@LAPTOP-HEF902DE>
2023-05-10 11:01:31 +0700

    add todo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
+import { type ChangeEventHandler, useState } from 'react'
+
 const IndexPage = () => {
-  return <div>Hello World</div>
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([
+    { id: 1, content: 'Todo#1' },
+    { id: 2, content: 'Todo#2' },
+    { id: 3, content: 'Todo#3' },
+  ])
+
+  const handleTodoFormChanged: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTodo(event.target.value)
+  }
+
+  const addTodo = () => {
+    setTodos([{ id: todos.length + 1, content: todo}, ...todos])
+    setTodo('')
+  }
+
+  return (
+    <>
+      <input type="text" onChange={handleTodoFormChanged} value={todo} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}>{todo.content}</li>)
+        }
+      </ul>
+    </>
+  )
 }
 
 export default IndexPage
-
-// JSX
